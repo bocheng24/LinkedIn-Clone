9efc74e64324a8b65662400e2fa9313ec88b37ee
bocheng24 <jeffcgp@hotmail.com>
2022-01-27 21:17:52 +0800

    Basic setup

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+  return (
+      <div>
+        <SearchIcon />
+      </div>
+  );
+}
+
+export default Header;
